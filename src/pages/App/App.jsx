197a63b15dfe6ac16d254d95e9045df47da99dae
LeonardoTavares1 <leonardao1234@gmail.com>
2022-09-21 19:44:27 -0300

    versao 00

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Div } from './Style.jsx';
+
+
+export function App(){
+    return(
+        <Div>
+            Very <br /> Flores
+        </Div>
+    )
+}
---
+++ src/pages/App/Style.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+
+const Div = styled.div`
+
+    font-size: 200px;
+    display: flex;
+    justify-content: center;
+    color: blue;
+
+`;
+
+export {Div};
